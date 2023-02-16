b15add4c2fbed5d3d209fc42a8dbbb7fbfad1819
alpha_devking <danielose266@gmail.com>
2023-02-16 09:48:54 +0100

    resolved the serializer problem for locating the campaign via slug content

--- backend/campaigns/urls.py
+++ backend/campaigns/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CampaignListAPIView
+from .views import CampaignListAPIView, CampaignDetailAPIView, SubscribeToCampaignAPIView
 
 urlpatterns = [
-    path('campaigns', CampaignListAPIView.as_view(), name='campaigns')
+    path('campaigns', CampaignListAPIView.as_view(), name='campaigns'),
+    path('campaigns/<str:slug>', CampaignDetailAPIView.as_view(), name='campaign-detail'),
+    path('campaigns/subscribe', SubscribeToCampaignAPIView.as_view(), name='subscribe')
 ]
